319210f5f52997ac7982e2cf6e872fc1f06e8b8f
Ricardo <rickbolar@gmail.com>
2016-06-22 18:02:41 -0600

    Se agrego las acciones resta division y multiplicacion

---
+++ Groupware.Calentamiento/Core.Numero/Dominio/Acciones/Division.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Numero.Dominio.Acciones
+{
+    class Division
+    {
+        public Numero Operar(Numero elPrimerNumero, Numero elSegundoNumero)
+        {
+            Numero resultado = null;
+            var validarBases = new Validaciones.ValidarBase();
+            if (validarBases.LasDosBasesSonIguales(elPrimerNumero, elSegundoNumero))
+            {
+                double elResultadoNumerico = (elPrimerNumero.elNumeroEnDecimal / elSegundoNumero.elNumeroEnDecimal);
+                resultado = new Numero(elResultadoNumerico.ToString(), elPrimerNumero.laBase);
+            }
+            return (resultado);
+
+        }
+    }
+}
